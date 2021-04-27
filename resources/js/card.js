d4c3981f450e1dd88ffb9f36bcd9941b354508cc
Clovis Leblond <clovisleblond@MacBook-Pro.local>
2021-04-27 02:01:22 -0400

    tests webpack

--- resources/js/card.js
+++ resources/js/card.js
@@ -1,7 +1,16 @@
 Nova.booting((Vue, router, store) => {
-    Vue.component('filterable-modal', require('./components/FilterModal'));
-    Vue.component('date-time-picker', require('./components/DateTimePicker'));
-    Vue.component('filterable-value-metric', require('./components/FilterableValueMetric'));
-    Vue.component('filterable-trend-metric', require('./components/FilterableTrendMetric'));
-    Vue.component('filterable-partition-metric', require('./components/FilterablePartitionMetric'));
-})
+    Vue.component("filterable-modal", require("./components/FilterModal"));
+    Vue.component("date-time-picker", require("./components/DateTimePicker"));
+    Vue.component(
+        "filterable-value-metric",
+        require("./components/FilterableValueMetric")
+    );
+    Vue.component(
+        "filterable-trend-metric",
+        require("./components/FilterableTrendMetric")
+    );
+    Vue.component(
+        "filterable-partition-metric",
+        require("./components/FilterablePartitionMetric")
+    );
+});
